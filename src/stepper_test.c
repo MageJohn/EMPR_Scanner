bffdeb85f443167289d79b211802e69b470c20b7
Yuri Pieters <yjtp500@york.ac.uk>
2020-01-22 16:01:21 +0000

    Add working stepper test

--- src/stepper_test.c
+++ src/stepper_test.c
@@ -1,6 +1,6 @@
 #include "lpc17xx_i2c.h"
+#include "lpc17xx_systick.h"
 #include "ioboard/i2c.h"
-#include "mbed/rit.h"
 #include "leds.h"
 #include "mbed/wait.h"
 
@@ -12,26 +12,32 @@ I2C_M_SETUP_Type packet = {
         .tx_data = states,
         .tx_length = 1
     };
+uint8_t led_state = 0;
 
 void main(void) {
     ioboard_i2c_init();
 
     led_setup();
-    led_disp_num(0xff);
 
-    mbed_rit_init();
-    mbed_rit_set(200e3);
-    mbed_rit_state(ENABLE);
-    while (1);
+    SYSTICK_InternalInit(1);
+    SYSTICK_IntCmd(ENABLE);
+    SYSTICK_Cmd(ENABLE);
+
+    return;
 }
 
-void RIT_IRQHandler(void) {
-    led_clear();
-    /*
-    I2C_MasterTransferData(LPC_I2C1, &packet, I2C_TRANSFER_POLLING);
-    packet.tx_data += 1;
-    if (packet.tx_data - states > 3) {
-        packet.tx_data = states;
+void SysTick_Handler(void) {
+    static uint8_t led_state = 0xff;
+    static uint8_t motor_counter = 0;
+    motor_counter += 1;
+    if (motor_counter > 200) {
+        motor_counter = 0;
+        led_clear();
+        led_disp_num(led_state ^= 0xff);
+        I2C_MasterTransferData(LPC_I2C1, &packet, I2C_TRANSFER_POLLING);
+        packet.tx_data += 1;
+        if ((packet.tx_data - states) > 3) {
+            packet.tx_data = states;
+        }
     }
-    */
 }
