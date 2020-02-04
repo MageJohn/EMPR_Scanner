b6841742a77b0d018c367c942ea48a5e1e4b0c00
MDJ Bassett <mdjb501@cse169pc-45.cs.york.ac.uk>
2020-02-04 16:46:01 +0000

    raster not working. needs fixing

--- src/raster_scan_B2.c
+++ src/raster_scan_B2.c
@@ -1,56 +1,76 @@
-#include "platform.h"
-#include "raster_scan_B2.h"
 
-//TODO: find best z height 
-/*
-DEFINE Z_HEIGHT 
-DEFINE X_LEFT
-DEFINE X_RIGHT
-DEFINE Y_END
-*/
+#include "platform.h"
+#include "mbed.h"
+#include "raster_scan_B2.h"
+#include "leds.h"
 
 // Ensure that step and end/start correlate so final step will end on end/start position
-#define X_START 50
+#define X_START 80
 #define Y_START 50
 #define Z_HEIGHT 10
-#define X_END 700
-#define X_STEP 10
-#define Y_STEP 50
-#define Y_END 500
-#define X_BEGIN 60
+
+struct LedSource *led;
 
 int main(void) {
-    raster_scan();
+    platform_init();
+    led_setup();
+    led = led_mux_register_source(20);
+    led_mux_set_curr(20);
+    platform_calibrate_head();
+    while(!platform_calibrated());
+    raster_scan(X_START, Y_START, Z_HEIGHT, 100, 20);
     return(1);
 
 }
 
-void raster_scan() {
-    platform_init();
-    platform_calibrate_head();
-    while (!platform_calibrated());
-    platform_head_set_coords(X_BEGIN, Y_START, Z_HEIGHT);
+//up to prgrammer to decide which dimension to scan. make stop_coord correspond.
+//dimension chooses where to scan. 1 = x, 2 = y
+bool one_dimensional_scan(uint16_t x, uint16_t y, uint16_t z, uint16_t stop_coord, uint8_t step) {
+    platform_head_set_coords(x, y, z);
     while(!platform_head_at_coords());
-    uint16_t current_y = Y_START;
-    uint16_t current_x = X_BEGIN;
-    uint16_t step_direction = X_STEP; //reverse when end reached
-    while (true) {
-        if (current_y == 500) {
-            break;
+    bool end = false;
+    while(!end) {
+        x += step;
+        if(x >= stop_coord) {
+            end = true;
         }
-        else if(current_x == X_START || current_x == X_END) {
-            step_direction *= -1;
-            current_y += Y_STEP;
-            platform_head_set_coords(current_x, current_y, Z_HEIGHT);
-            while(!platform_head_at_coords());
-            current_x += step_direction;
-            platform_head_set_coords(current_x,current_y,Z_HEIGHT);
-            while(!platform_head_at_coords());
+        else if (x <= stop_coord) {
+            end = true;
+        }
+        platform_head_set_coords(x, y, z);
+        while(!platform_head_at_coords());
+        wait_ms(500);
+        //do sensor stuff here
+    }
+    return true;
+}
+
+
+//void detect_flag(int16_t mid_x, uint16_t mid_y, uint16_t z, uint16_t *x_var, uint16_t *y_var) {}
+
+void raster_scan(uint16_t x, uint16_t y, uint16_t z, uint16_t grid_size_x, uint16_t grid_size_y) {
+    //starting_position(x, y, z);
+    uint16_t start_x = x;
+    uint16_t stop_x = X_SOFT_LIMIT;
+    uint16_t x_step = (X_SOFT_LIMIT - x)/grid_size_x;
+    uint16_t y_step = (Y_SOFT_LIMIT - y)/grid_size_y;
+    while(true) {
+        if (y >= Y_SOFT_LIMIT) {
+            platform_head_set_coords(0, 0, 0);
+            break;
+        } else if(x == stop_x) {
+            one_dimensional_scan(stop_x, y, z, start_x, x_step);
+            x = start_x;
+            x_step *= -1;
         }
         else {
-            current_x += step_direction;
-            platform_head_set_coords(current_x,current_y,Z_HEIGHT);
-            while(!platform_head_at_coords());
+            one_dimensional_scan(start_x, y, z, stop_x, x_step);
+            x = stop_x;
+            x_step *= -1;
         }
+        y += y_step;
+        led->num = y/10;
+        //platform_head_set_coords(x,y,z);
+        //while(!platform_head_at_coords());
     }
 }
