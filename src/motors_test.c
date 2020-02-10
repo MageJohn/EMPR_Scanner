9a708c97c9daca8f9bd2072b496dbf94eece289b
Yuri Pieters <yjtp500@york.ac.uk>
2020-02-10 11:49:59 +0000

    Remove extra line from bad merge

--- src/motors_test.c
+++ src/motors_test.c
@@ -10,6 +10,5 @@ void main(void) {
 
     platform_calibrate_head();
     while (!platform_calibrated());
-    platform_head_set_coords(X_SOFT_LIMIT - 1, Y_SOFT_LIMIT -1, 500);
     platform_head_set_coords(0, 100, 100);
 }
