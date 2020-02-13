04e1f6ad815e61b677f9e372c0b4731444489078
Yuri Pieters <magejohnyjtp@gmail.com>
2020-02-13 12:29:26 +0000

    Add a serial non-blocking write
    Also a note on what 'non-blocking' means here.

--- lib/serial.h
+++ lib/serial.h
@@ -5,6 +5,8 @@ uint32_t serial_read_nb(char *buf, uint32_t length);
 
 uint32_t serial_read_b(char *buf, uint32_t length);
 
+uint32_t serial_write_nb(char *buf, uint32_t length);
+
 uint32_t serial_write_b(char *buf, uint32_t length);
 
 void serial_init(void);
