8bdc4159d1417c6dd3a4eff8b7b087510e88c714
Dan <dkp508@york.ac.uk>
2020-03-05 12:22:25 +0000

    fix rgb vals

--- src/python/receive_data_D1.py
+++ src/python/receive_data_D1.py
@@ -9,8 +9,8 @@ with serial.Serial('/dev/ttyACM0', 9600) as ser:
         x_y_z_vals = unpack("<3H", x_y_z)
         r_g_b_vals = unpack("<4H", r_g_b)
 
-        r_g_b_final = [int((chan / 700) * 255) for chan in r_g_b_vals]
-
+        r_g_b_final = [int((float(chan) / 700) * 255) for chan in r_g_b_vals]
+        
         print("Coord=" + str(x_y_z_vals))
         print("RGB=" + str(r_g_b_final))
         
